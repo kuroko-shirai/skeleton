a4ab8c8573fdb99434c51fc1f605701db7b55244
a.semyannikov@automacon.ru <a.semyannikov@automacon.ru>
2025-01-18 02:28:04 +0300

    init

--- internal/repositories/service/service.go
+++ internal/repositories/service/service.go
@@ -4,10 +4,13 @@ import (
 	"context"
 )
 
+// Service: used for any services which should be running.
 type Service interface {
 	Up(context.Context) error
 }
 
+// ServiceWithDown: used for any services which should be
+// running and safly ended.
 type ServiceWithDown interface {
 	Service
 
